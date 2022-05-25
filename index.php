12812a15e99ceb921651e5f3c0ae22f12b45a74c
hl42751 <104617618+hl42751@users.noreply.github.com>
2022-05-25 15:39:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+      $n =2;
+      switch($n){
+        case 2;
+            echo 'thu 2';
+            break;
+        case 3;
+            echo 'thu 3';
+            break;
+        case 4;
+            echo 'thu 4';
+            break;    
+        case 5;
+            echo 'thu 5';
+            break;
+        case 6;
+            echo 'thu 6';
+            break;
+        case 7;
+            echo 'thu 7';
+            break;
+        default:
+            echo 'Khong phai ngay trong tuan';
+      }
+?>
